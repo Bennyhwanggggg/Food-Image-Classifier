660ffd3fe72206b2d17a5cef9ce2e44d5a12e203
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-09-29 06:37:05 +1000

    update helper to save models

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+
+target_dir = './models/'
+
+
+def save_model(model, filename='{}.h5'.format(datetime.datetime.now().strftime("%d_%m_%Y_%H_%M"))):
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    model_file = '{}models_{}'.format(target_dir, filename)
+    weights_file = '{}weights_{}'.format(target_dir, filename)
+    model.save(model_file)
+    model.save_weights(weights_file)
