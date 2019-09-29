2d6c9a9eac59981db0826aba1cd2f8c6b1e7125f
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-09-29 21:15:45 +1000

    fix deploy port issue

--- web/server.py
+++ web/server.py
@@ -67,4 +67,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
