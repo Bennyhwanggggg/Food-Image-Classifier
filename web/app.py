23386bc0574bff8ce2781918c6de67ded757d420
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-10-03 01:04:50 +1000

    fix port

--- web/app.py
+++ web/app.py
@@ -98,5 +98,5 @@ def predict():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("$PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
