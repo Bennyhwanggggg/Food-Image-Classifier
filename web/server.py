466e614b233a959ba27350b01374fc630a6494e9
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-10-02 22:19:29 +1000

    add deploy stuff

--- web/server.py
+++ web/server.py
@@ -15,16 +15,31 @@ from keras.preprocessing import image as image_utils
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-import requests
+from web.file_manager import FileManager
 from werkzeug.utils import secure_filename
 
 
+# model files setup
+file_manager = FileManager()
+
+# local
 # model_path = '../models/models_29_09_2019_18_32.h5'
 # weights_path = '../models/weights_29_09_2019_18_32.h5'
-model_path = '../models/models_29_09_2019_18_50.h5'
-model_path = os.path.join(PATH, model_path)
-weights_path = '../models/weights_29_09_2019_18_50.h5'
-weights_path = os.path.join(PATH, weights_path)
+# model_path = '../models/models_29_09_2019_18_50.h5'
+# model_path = os.path.join(PATH, model_path)
+# weights_path = '../models/weights_29_09_2019_18_50.h5'
+# weights_path = os.path.join(PATH, weights_path)
+
+# S3
+model_file_to_download = 'models_29_09_2019_18_50.h5'
+weights_file_to_download = 'weights_29_09_2019_18_50.h5'
+model_file_name = './models.h5'
+weights_file_name = './weights.h5'
+model_path = os.path.join(PATH, model_file_name)
+weights_path = os.path.join(PATH, weights_file_name)
+
+file_manager.download_file(model_file_to_download, model_path)
+file_manager.download_file(weights_file_to_download, weights_path)
 
 labels_file_path = os.path.join(PATH, 'labels.txt')
 labels = np.loadtxt(labels_file_path, delimiter='\n', dtype=str)
@@ -44,7 +59,6 @@ def predict():
     if 'file' not in request.files:
         return jsonify(message='error no file'), 400
     file = request.files['file']
-    print(type(file))
     if not file.filename:
         return jsonify(message='error no file name'), 400
     # if file and allowed_file(file.filename):
